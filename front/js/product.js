30a0afca74e7972de03b3687fba786c7408bdba6
Yannick Cathala <cyansweb@gmail.com>
2022-11-07 16:17:20 +0100

    Storage

--- front/js/product.js
+++ front/js/product.js
@@ -46,3 +46,52 @@ fetch("http://localhost:3000/api/products/" + idProduct)
     .catch(function(error) {
         console.error(error)
     })
+
+
+//------------------------------------------ Stokage des produits dans le LocalStorage ------------------------------------------//
+
+const retourApi = [{
+        _id: "ijsdfiojsodfj7df",
+        title: "Un premier produit",
+        price: 12
+    },
+    {
+        _id: "ijsdfiojsqsdqsd7df",
+        title: "Un deuxième produit",
+        price: 50
+    }
+];
+
+function recupererPanier() {
+    const panier = [];
+
+    if (!localStorage.getItem("panier")) {
+        localStorage.setItem("panier", JSON.stringify(panier));
+    }
+
+    return JSON.parse(localStorage.getItem("panier"));
+}
+
+function sauvegarderPanier(nouveauPanier) {
+    localStorage.setItem("panier", JSON.stringify(nouveauPanier));
+}
+
+function ajouterProduitAuPanier(nouveauProduit) {
+    const nouveauProduitDuPanier = {
+        _id: nouveauProduit._id,
+        title: nouveauProduit.title
+    };
+
+    const panier = recupererPanier();
+    const produitExistant = panier.filter(produit => produit._id === nouveauProduit._id);
+
+    if (produitExistant.length > 0) {
+        console.log("j'ai déjà ce produit !");
+        return;
+    }
+
+    panier.push(nouveauProduitDuPanier);
+    sauvegarderPanier(panier);
+}
+
+ajouterProduitAuPanier(retourApi[0]);
